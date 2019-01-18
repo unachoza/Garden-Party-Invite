089d38e20da5151d62e40f9bc813d209e07aa1e1
Arianna Choza <unachoza@gmail.com>
2019-01-18 15:43:10 -0800

    longest annimation filled with timeouts

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // alert('let us party')
+
+
+setTimeout(function(){document.getElementById("header").innerHTML="You're invited to this party!";}, 8500);
+// clearTimeout(function(){document.getElementById("header").innerHTML="You're invited to this party!";}, 1000);
+setTimeout(function() {document.getElementById('header').style.display='none'}, 10000)
+// setTimeout(function() {document.getElementById('details').innerHTML='It\'s a Garden Party theme'}, 10000)
+setTimeout(function() {document.getElementById('header').style.display='none'}, 40000)
+
+// setTimeout(function(){document.getElementById("title1").style.visibility="visible"}, 10000);
+setTimeout(function(){document.getElementById("title2").style.visibility="visible"}, 12000);
+setTimeout(function(){document.getElementById("title3").style.visibility="visible"}, 12000);
+setTimeout(function(){document.getElementById("title4").style.visibility="visible"}, 12000);
+setTimeout(function(){document.getElementById("title5").style.visibility="visible"}, 12000);
+setTimeout(function(){document.getElementById("title6").style.visibility="visible"}, 12000);
+setTimeout(function(){document.getElementById("when").innerHTML="Saturday 1/19";}, 13000);
+setTimeout(function(){document.getElementById("time").innerHTML="7:30pm";}, 14000);
+setTimeout(function(){document.getElementById("where").innerHTML="Del Mar";}, 15000);
+setTimeout(function(){document.getElementById("where2").innerHTML="3605 Bernwood Pl";}, 15000);
+setTimeout(function(){document.getElementById("where3").innerHTML="San Diego, CA 92130";}, 15000);
+setTimeout(function(){document.getElementById("dress").innerHTML="ladies: florals; ";}, 16000);
+setTimeout(function(){document.getElementById("dress2").innerHTML="men: improvise; ";}, 16000);
+setTimeout(function(){document.getElementById("occassion").innerHTML="I really like to throw parties";}, 17000);
